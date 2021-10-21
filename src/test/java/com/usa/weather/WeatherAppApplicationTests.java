36e35a88a0f62c19043843e5bf94324376540447
panditra <45869640+panditra@users.noreply.github.com>
2021-10-21 14:56:23 +0530

    hello4

--- src/test/java/com/usa/weather/WeatherAppApplicationTests.java
+++ src/test/java/com/usa/weather/WeatherAppApplicationTests.java
@@ -2,7 +2,7 @@ package com.usa.weather;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//hello
+//hello4
 @SpringBootTest
 class WeatherAppApplicationTests {
 
