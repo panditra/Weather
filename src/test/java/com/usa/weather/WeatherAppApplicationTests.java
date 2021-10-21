2d0b0337abbd72a82547fd6249d474346b54d068
panditra <45869640+panditra@users.noreply.github.com>
2021-10-21 15:43:01 +0530

    new 3 branch updated

--- src/test/java/com/usa/weather/WeatherAppApplicationTests.java
+++ src/test/java/com/usa/weather/WeatherAppApplicationTests.java
@@ -2,7 +2,7 @@ package com.usa.weather;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//in new3 bracnh
 @SpringBootTest
 class WeatherAppApplicationTests {
 
